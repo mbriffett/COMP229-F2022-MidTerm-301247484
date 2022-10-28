4b42b8c01cf1d71bfc130fe8618c648a2df8087c
mbriffett <matthewbriffett@gmail.com>
2022-10-28 09:23:18 -0230

    Delete and buttons in list view

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -4,19 +4,19 @@ import booksModel from '../models/books.js';
 /* GET books List page. READ */
 export function displayBookList(req, res, next) {
     // find all books in the books collection
-    booksModel.find((err, booksCollection) => {
+    booksModel.find(function(err, booksCollection){
         if (err) {
             console.error(err);
             res.end(err);
         }
         res.render('index', { title: 'Book List', page: 'books/list', books: booksCollection });
-    });
+    })
 }
 
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
 
-    res.render('index', { title: 'Add Book', page: 'books/add', books: booksCollection });
+    res.render('index', { title: 'Add Book', page: 'books/add', books: {} });
 }
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -37,7 +37,7 @@ export function processAddPage(req, res, next) {
             res.end(err);
         };
 
-        res.redirect('/books/add')
+        res.redirect('/books/list')
     })
  }
 
@@ -60,6 +60,7 @@ export function displayEditPage(req, res, next) {
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
     let newBook = booksModel({
+        _id: req.body.id,
         name: req.body.name,
         author: req.body.author,
         published: req.body.published,
@@ -68,7 +69,7 @@ export function processEditPage(req, res, next) {
        
     });
 
-    booksModel.updateOne(newBook, (err, book) => {
+    booksModel.updateOne({ _id: req.body.id }, newBook, (err, book) => {
         if (err) {
             console.error(err);
             res.end(err);
@@ -80,7 +81,14 @@ export function processEditPage(req, res, next) {
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    let id = req.params.id;
+
+    booksModel.updateOne({ _id: id }, (err, book) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/books/list')
+    })
 }
